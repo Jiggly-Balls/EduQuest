1c1d947e7991b31ad5e17e9ba9f338d27c7364c1
Jiggly Balls <jigglyballs9000@gmail.com>
2025-09-17 23:14:08 +0530

    Added card component

--- src/componenets/Cards.tsx
+++ src/componenets/Cards.tsx
@@ -1,10 +1,17 @@
-function GenerateCard(title: String, text: String) {
+type CardProps = {
+  image: string;
+  title: string;
+  text: string;
+};
+
+
+function GenerateCard({ image, title, text }: CardProps) {
   return (
       <div className="card">
+        <img className="card-img-top" src={ image }></img>
         <div className="card-body">
           <h5 className="card-title">{ title }</h5>
           <p className="card-text">{ text }</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
       </div>
   );

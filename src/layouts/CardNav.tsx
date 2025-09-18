ca688d03b48388cfca36e90070daf1b7468fc7c5
Jiggly Balls <jigglyballs9000@gmail.com>
2025-09-18 17:28:06 +0530

    Updated import path

--- src/layouts/CardNav.tsx
+++ src/layouts/CardNav.tsx
@@ -1,7 +1,7 @@
 import React, { useLayoutEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { GoArrowUpRight } from "react-icons/go";
-import "../styles/CardNav.css";
+import "../Styles/CardNav.css";
 
 type CardNavLink = {
   label: string;

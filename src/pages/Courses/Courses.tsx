56d29958b1cde9278be8e7c21e22171b3b1d49e6
Jiggly Balls <jigglyballs9000@gmail.com>
2025-09-18 22:16:28 +0530

    Added Card & centered the title

--- src/pages/Courses/Courses.tsx
+++ src/pages/Courses/Courses.tsx
@@ -1,9 +1,14 @@
 import SplitText from "../../Componenets/SplitText";
+import GenerateCard from "../../Componenets/Cards";
+
+import pythonLogo from "../../assets/python-logo.png";
+
+import "../../Styles/Pages/Courses/Courses.css";
 
 function Courses() {
   return (
     <div>
-      <h1>
+      <h1 className="Title justify-content-center">
         <SplitText
           text="Courses"
           className="text-2xl font-semibold text-center justify-content-center"
@@ -18,6 +23,16 @@ function Courses() {
           textAlign="center"
         />
       </h1>
+      <div
+        id="courses-cards"
+        className="d-flex flex-row mb-3 justify-content-center"
+      >
+        <GenerateCard
+          image={pythonLogo}
+          title="Python Programming"
+          text="Learn Python Programming"
+        />
+      </div>
     </div>
   );
 }

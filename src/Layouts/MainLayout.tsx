1365f9c51ea9b6237d20d54f16f280e8e6a234d9
Jiggly Balls <jigglyballs9000@gmail.com>
2025-09-18 18:40:58 +0530

    Added main page layout

---
+++ src/Layouts/MainLayout.tsx
@@ -0,0 +1,75 @@
+import { Outlet } from "react-router-dom";
+
+import CardNav from "../Componenets/CardNav";
+import logo from "../assets/FTF.png";
+
+import "../Styles/MainLayout.css";
+
+const items = [
+  {
+    label: "About",
+    bgColor: "#41265eff",
+    textColor: "#fff",
+    links: [
+      {
+        label: "Home Page",
+        href: "#/",
+        ariaLabel: "Home Page",
+      },
+      {
+        label: "Our Mission",
+        href: "https://google.com",
+        ariaLabel: "About us",
+      },
+    ],
+  },
+  {
+    label: "Courses",
+    bgColor: "#41265eff",
+    textColor: "#fff",
+    links: [
+      {
+        label: "Python",
+        href: "#/courses/python",
+        ariaLabel: "Python Course",
+      },
+      {
+        label: "All Courses",
+        href: "#/courses",
+        ariaLabel: "All Courses",
+      },
+    ],
+  },
+  {
+    label: "Contact",
+    bgColor: "#41265eff",
+    textColor: "#fff",
+    links: [
+      { label: "Email", href: "https://google.com", ariaLabel: "Email us" },
+      { label: "Twitter", href: "https://google.com", ariaLabel: "Twitter" },
+    ],
+  },
+];
+
+function MainLayout() {
+  return (
+    <>
+      <CardNav
+        logo={logo}
+        logoAlt="Company Logo"
+        items={items}
+        baseColor="#755697ff"
+        menuColor="#d0a4ffff"
+        buttonBgColor="#000000ff"
+        buttonTextColor="#fff"
+        ease="power3.out"
+      />
+      <div id="nav-margin" />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default MainLayout;

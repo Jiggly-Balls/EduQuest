81989d9086e92fb9c7678e0d43a5a74b5385c8ac
Jiggly Balls <jigglyballs9000@gmail.com>
2025-09-18 18:40:38 +0530

    Added more routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,23 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Courses from "./Pages/Courses/Courses";
+import MainLayout from "./Layouts/MainLayout";
+import Python from "./Pages/Courses/Python/Python";
+import PythonIntroduction from "./Pages/Courses/Python/PythonIntroduction";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/courses/python" element={<Python />} />
+          <Route
+            path="/courses/python/introduction"
+            element={<PythonIntroduction />}
+          />
+        </Route>
       </Routes>
     </HashRouter>
   );

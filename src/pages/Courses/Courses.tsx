4e92293a8f9cb11cbe52f18971a692827836903e
Jiggly Balls <jigglyballs9000@gmail.com>
2025-09-18 19:34:41 +0530

    Added Courses header

--- src/pages/Courses/Courses.tsx
+++ src/pages/Courses/Courses.tsx
@@ -1,5 +1,25 @@
+import SplitText from "../../Componenets/SplitText";
+
 function Courses() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>
+        <SplitText
+          text="Courses"
+          className="text-2xl font-semibold text-center justify-content-center"
+          delay={125}
+          duration={0.6}
+          ease="power3.out"
+          splitType="chars"
+          from={{ opacity: 0, y: 40 }}
+          to={{ opacity: 1, y: 0 }}
+          threshold={0.1}
+          rootMargin="-100px"
+          textAlign="center"
+        />
+      </h1>
+    </div>
+  );
 }
 
 export default Courses;

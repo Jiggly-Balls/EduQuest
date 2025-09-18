f3512ed3359b0649d6b5834b38d075a4856de36b
Jiggly Balls <jigglyballs9000@gmail.com>
2025-09-18 22:14:38 +0530

    Added optional buttons to GenerateCard

--- src/componenets/Cards.tsx
+++ src/componenets/Cards.tsx
@@ -2,15 +2,28 @@ type CardProps = {
   image: string;
   title: string;
   text: string;
+  buttonText?: string;
+  buttonLink?: string;
 };
 
-function GenerateCard({ image, title, text }: CardProps) {
+function GenerateCard({
+  image,
+  title,
+  text,
+  buttonText,
+  buttonLink,
+}: CardProps) {
   return (
     <div className="card">
       <img className="card-img-top" src={image}></img>
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{text}</p>
+        {buttonText && buttonLink && (
+          <a href={buttonLink} className="btn btn-primary">
+            {buttonText}
+          </a>
+        )}
       </div>
     </div>
   );

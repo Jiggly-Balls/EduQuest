aa7ecb6b4d45ceb0be14faea7d27efa621e1795c
Jiggly Balls <jigglyballs9000@gmail.com>
2025-09-19 18:20:23 +0530

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // ... other configurations
+  prefix: "tw-", // Add 'tw-' prefix to all Tailwind utility classes
+  corePlugins: {
+    preflight: false, // Disable Tailwind's Preflight base styles
+  },
+};

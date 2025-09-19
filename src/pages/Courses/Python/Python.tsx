54afeee41b210ce226ad314a0c96a050fba3e5a4
Jiggly Balls <jigglyballs9000@gmail.com>
2025-09-19 22:36:40 +0530

    Automated table of contents

--- src/pages/Courses/Python/Python.tsx
+++ src/pages/Courses/Python/Python.tsx
@@ -2,6 +2,17 @@ import SplitText from "../../../Componenets/SplitText";
 import pythonLogo from "../../../assets/python-logo-transparent.png";
 import "../../../Styles/Pages/Courses/Python/Python.css";
 
+const contentPages = {
+  Introduction: "#/courses/python/introduction",
+  Variables: "#/courses/",
+  "Data Types": "#/courses/",
+  "Arithmetic Operators": "#/courses/",
+  "Comparison Operators": "#/courses/",
+  "Logical Operators": "#/courses/",
+  "Identity Operators": "#/courses/",
+  "Membership Operators": "#/courses/",
+};
+
 function Python() {
   return (
     <div>
@@ -29,7 +40,7 @@ function Python() {
         <SplitText
           text="Python is a high-level, interpreted programming language known for its simplicity, readability, and versatility. It supports multiple programming paradigms, including object-oriented, functional, and procedural styles. Python's extensive standard library and large ecosystem of third-party packages make it widely used in fields like web development, data science, artificial intelligence, automation, and scientific computing. Its beginner-friendly syntax and strong community support have made it one of the most popular programming languages in the world."
           className="text-2xl font-semibold text-center justify-content-center"
-          delay={15}
+          delay={10}
           duration={0.1}
           ease="power3.out"
           splitType="chars"
@@ -41,37 +52,33 @@ function Python() {
         />
       </p>
 
-      <table
-        id="table-of-contents"
-        className="table table-dark justify-content-center"
+      <div
+        id="table-of-contents-header"
+        className="d-flex flex-row mb-3 justify-content-center"
       >
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Topic Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>
-              <a href="#/courses/python/introduction">Introduction</a>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>
-              <a href="#/courses/">Variables</a>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>
-              <a href="#/courses/">Variables</a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+        <h2>Table of Contents</h2>
+      </div>
+
+      <div className="d-flex flex-row mb-3 justify-content-center">
+        <table id="table-of-contents" className="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Topic Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.entries(contentPages).map(([key, value], index) => (
+              <tr key={key}>
+                <th scope="row">{index + 1}</th>
+                <td>
+                  <a href={value}>{key}</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }

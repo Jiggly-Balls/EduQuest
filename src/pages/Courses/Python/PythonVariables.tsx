64ce27a6c22dd6bd3ed459c324db6a427270ebb5
Jiggly Balls <jigglyballs9000@gmail.com>
2025-09-21 15:29:29 +0530

    Finished variables page

--- src/pages/Courses/Python/PythonVariables.tsx
+++ src/pages/Courses/Python/PythonVariables.tsx
@@ -1,6 +1,12 @@
+import PythonCodeBlock from "../../../Componenets/CodeBlock";
 import SplitText from "../../../Componenets/SplitText";
+import VariableDragGame from "../../../Utilities/DNDGame";
 
 import "../../../Styles/Pages/Courses/Python/PythonVariables.css";
+
+const variableExample = `age = 19
+name = "Krish"
+is_above_18 = True`;
 
 function PythonVariables() {
   return (
@@ -21,15 +27,81 @@ function PythonVariables() {
         />
       </h1>
       <div className="page-content">
-        In Python, a variable is simply a name that refers to a value stored in
-        memory. Unlike some other programming languages, you don't need to
-        declare a variable's type explicitly—Python automatically determines the
-        type based on the value you assign. This makes Python dynamically typed.
-        <br />
-        <br />
-        Variables act like containers that hold data, and the value they store
-        can change during program execution. You create a variable by using the
-        assignment operator =
+        <p>
+          In Python, a variable is simply a name that refers to a value stored
+          in memory. Unlike some other programming languages, you don't need to
+          declare a variable's type explicitly—Python automatically determines
+          the type based on the value you assign. This makes Python dynamically
+          typed.
+          <br />
+          <br />
+          Variables act like containers that hold data, and the value they store
+          can change during program execution. You create a variable by using
+          the assignment operator <code>=</code>
+          <br />
+          <br /> Example-
+        </p>
+        <PythonCodeBlock code={variableExample} />
+        <p>
+          <br />
+          <br />
+          This code block demonstrates how you can create variables in python!
+          <br />
+          Over here <code>age</code>, <code>name</code> and{" "}
+          <code>is_above_18</code> are called <code>variables</code> and{" "}
+          <code>19</code>, <code>"Krish"</code> and <code>True</code> are called
+          as <code>values</code> or <code>data</code>, of the respective
+          variables.
+          <br />
+          <br />
+          This means we are assigning a block of the computer's memory the label{" "}
+          <code>name</code>, and storing the data <code>"Krish"</code> in it
+          (the same for other other two variables also).
+          <br />
+          <br />
+          In python there are certain rules we need to follow while naming a
+          variable. They are-
+        </p>
+        <table id="variable-name-rules" className="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Rules</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Variables cannot start with a number</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>
+                Variables cannot have any special characters in them EXCEPT
+                underscores <code>_</code>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Variables cannot have spaces</td>
+            </tr>
+            <tr>
+              <th scope="row">4</th>
+              <td>
+                Variables cannot have the same name as{" "}
+                <a href="https://docs.python.org/3/reference/lexical_analysis.html#keywords">
+                  keywords
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div
+          id="variable-game"
+          className="d-flex flex-row mb-3 justify-content-center"
+        >
+          <VariableDragGame />
+        </div>
       </div>
     </div>
   );

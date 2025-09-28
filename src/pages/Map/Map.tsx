31dfc53fdbede562e92b0c85a039b27f886620bf
Jiggly Balls <jigglyballs9000@gmail.com>
2025-09-28 18:41:15 +0530

    Removed related python images & added road map

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,7 +1,4 @@
 import SplitText from "../../Componenets/SplitText";
-import GenerateCard from "../../Componenets/Cards";
-
-import pythonLogo from "../../assets/python-logo.png";
 
 import "../../Styles/Pages/Courses/Courses.css";
 
@@ -10,7 +7,7 @@ function Map() {
     <div>
       <h1 className="Title justify-content-center">
         <SplitText
-          text="Courses"
+          text="Map"
           className="text-2xl font-semibold text-center justify-content-center"
           delay={125}
           duration={0.6}
@@ -23,18 +20,6 @@ function Map() {
           textAlign="center"
         />
       </h1>
-      <div
-        id="courses-cards"
-        className="d-flex flex-row mb-3 justify-content-center"
-      >
-        <GenerateCard
-          image={pythonLogo}
-          title="Python Programming"
-          text="Learn Python Programming"
-          buttonText="Learn now"
-          buttonLink="#/courses/python"
-        />
-      </div>
     </div>
   );
 }
